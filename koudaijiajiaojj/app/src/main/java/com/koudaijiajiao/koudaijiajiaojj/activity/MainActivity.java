e76e08f8f9f73baaf93766c89bc648424500ca7d
Ytsssss <995170811@qq.com>
2016-10-28 15:57:31 +0800

    fix

--- koudaijiajiaojj/app/src/main/java/com/koudaijiajiao/koudaijiajiaojj/activity/MainActivity.java
+++ koudaijiajiaojj/app/src/main/java/com/koudaijiajiao/koudaijiajiaojj/activity/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ButtonBarLayout;
+
 import android.view.View;
 import android.widget.Button;
 
 import com.koudaijiajiao.koudaijiajiaojj.R;
+
 
 public class MainActivity extends Activity {
 
