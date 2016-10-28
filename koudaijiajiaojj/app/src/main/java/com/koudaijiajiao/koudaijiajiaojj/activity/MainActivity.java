db163c2a028ef9c838c54c6e5f1b531afc6c9530
killandy@163.com <killandy@KillAndydeMacBook-Pro.local>
2016-10-28 16:13:50 +0800

    actionbar

--- koudaijiajiaojj/app/src/main/java/com/koudaijiajiao/koudaijiajiaojj/activity/MainActivity.java
+++ koudaijiajiaojj/app/src/main/java/com/koudaijiajiao/koudaijiajiaojj/activity/MainActivity.java
@@ -2,36 +2,94 @@ package com.koudaijiajiao.koudaijiajiaojj.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.koudaijiajiao.koudaijiajiaojj.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    public Button btn_addcourse;
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+    private ArrayList<Fragment> fragments;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        initListener();
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC
+                );
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_home_white_24dp, "首页").setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_book_white_24dp, "发现").setActiveColorResource(R.color.teal))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_music_note_white_24dp, "我的").setActiveColorResource(R.color.blue))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
+        fragments = getFragments();
+        setDefaultFragment();
+        bottomNavigationBar.setTabSelectedListener(this);
     }
 
-    private void initListener() {
-        btn_addcourse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,AddcourseActivity.class);
-                startActivity(intent);
+    /** * 设置默认的 */
+    private void setDefaultFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.layFrame, HomeFragment.newInstance("Home"));
+        transaction.commit();
+    }
+
+    private ArrayList<Fragment> getFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(HomeFragment.newInstance("Home"));
+        fragments.add(BookFragment.newInstance("Books"));
+        fragments.add(MusicFragment.newInstance("Music"));
+        fragments.add(TvFragment.newInstance("Movies & TV"));
+        fragments.add(GameFragment.newInstance("Games"));
+        return fragments;
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                if (fragment.isAdded()) {
+                    ft.replace(R.id.layFrame, fragment);
+                } else {
+                    ft.add(R.id.layFrame, fragment);
+                }
+                ft.commitAllowingStateLoss();
             }
-        });
+        }
+
     }
 
-    private void initView() {
-        btn_addcourse= (Button) findViewById(R.id.btn_main_addcourse);
+    @Override
+    public void onTabUnselected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                ft.remove(fragment);
+                ft.commitAllowingStateLoss();
+            }
+        }
     }
 
+    @Override
+    public void onTabReselected(int position) {
+
+    }
 }
